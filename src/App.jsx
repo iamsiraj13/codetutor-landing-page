5681346f3bf41cd079affab9078e9da310a077f7
iamsiraj13 <iamsiraj13@gmail.com>
2025-02-17 17:15:42 +0600

    project initilize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h2 className="underline text-center text-5xl font-medium text-purple-600 mt-8">
+        Tazmin Akter Tania
+      </h2>
+    </div>
+  );
+};
+
+export default App;
